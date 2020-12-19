1e3590f2f8051793a264d66cc8b014f868218bf4
noelleTchu <55718901+noelleTchu@users.noreply.github.com>
2020-12-19 03:29:38 -0800

    Organized scenes, pseudocoded zombie script

---
+++ HiScore/Assets/Scripts/BaseInvaders/Level 1/ZombieScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        /*
+        //Initiate first wave of zombies
+
+        for number of columns
+            for number of rows
+                instantiate regular zombie
+        */   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        //Columns move left at set time period
+        How to do this?
+
+            //When column moves left add new column of zombies (endlessly or specified number of columns?)
+            for number of rows
+                call NewZombie to instantiate a regular or tanky zombie
+
+        */
+    }
+    /*
+    void NewZombie()
+    {
+        //Random number generator 0 - 10
+        if number is 8 or below
+            instantiate regular zombie
+        else
+            instantiate tanky zombie
+    }
+    */
+}
