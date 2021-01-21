75a7c5cc31c02bd7dcfe423c83fcc3a844368b5a
Abdulqader Koshak <abdulqaderkoshak@sandiego.edu>
2021-01-22 01:16:15 +0300

    Update ZombieMovement.cs

--- HiScore/Assets/Scripts/Pac-Man/ZombieMovement.cs
+++ HiScore/Assets/Scripts/Pac-Man/ZombieMovement.cs
@@ -40,13 +40,13 @@ public class ZombieMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Physics2D.queriesStartInColliders = true;
+        Physics2D.queriesStartInColliders = false;
         // raycast
         RaycastHit2D hit2D = Physics2D.Raycast(transform.position, currentDir, rayDistance, rayLayer);
         Vector3 endpoint = currentDir * rayDistance;
         // visably debug the ray
         Debug.DrawLine(transform.position, transform.position + endpoint, Color.green);
-        print(hit2D.collider);
+        
         // if walls and pacman layer are selected, will return true for either
         if (hit2D.collider != null)
         {
