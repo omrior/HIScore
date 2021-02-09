fb25d9637c82b8948945497046cd7b19e99f5048
Abdulqader Koshak <abdulqaderkoshak@sandiego.edu>
2021-02-10 02:56:29 +0300

    map script

---
+++ HiScore/Assets/Scripts/Pac-Man/map.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class map : MonoBehaviour
+{
+    public GameObject[] gameObjects;
+    
+    void Awake()
+    {
+       gameObjects = GameObject.FindGameObjectsWithTag("pacdot");
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        activateObjects();
+    }
+
+    public void activateObjects()
+    {
+
+        bool allInActive = true;
+
+        foreach (GameObject go in gameObjects)
+        {
+            if (go.activeInHierarchy)
+            {
+                allInActive = false;
+                break;
+            }
+        }
+        
+        if (allInActive)
+        {
+            foreach (GameObject go in gameObjects)
+            {
+                go.SetActive(true);
+            }
+        }
+        
+    }
+}
