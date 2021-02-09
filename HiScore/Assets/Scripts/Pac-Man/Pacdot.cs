579491c49c4d7d46a87a5262c2ef10c5d04a3695
omrior <55718926+omrior@users.noreply.github.com>
2021-02-09 15:16:41 -0800

    Pacdots and script added
    Added some pacdots for testing. Added a script for the pacdots.

---
+++ HiScore/Assets/Scripts/Pac-Man/Pacdot.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pacdot : MonoBehaviour
+{
+    void onTriggerEnter2D(Collider2D co) {
+    	if(co.name == "Hero") {
+    		Destroy(gameObject);
+    	}
+    }
+    
+}
