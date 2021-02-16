ce4c34f52eccf10b99543088780a75b5ecf6c039
omrior <55718926+omrior@users.noreply.github.com>
2021-02-16 15:47:38 -0800

    Scoring System
    Added scoring text.
    Added scoring script.

--- HiScore/Assets/Scripts/Pac-Man/Pacdot.cs
+++ HiScore/Assets/Scripts/Pac-Man/Pacdot.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class pacdot : MonoBehaviour
+public class Pacdot : MonoBehaviour
 {
 
     void OnTriggerEnter2D(Collider2D co)
     {
+    	ScoringScript.scoreValue += 10;
         print(co.name);
         if (co.name == "Hero")
         {
