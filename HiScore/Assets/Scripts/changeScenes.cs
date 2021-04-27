25e00c3d5dc6955976aad0732e78c8589e3d172d
omrior <55718926+omrior@users.noreply.github.com>
2021-04-27 15:52:31 -0700

    Retry button
    Retry button restarts the game now.
    Need to reset SI lives upon restart.

--- HiScore/Assets/Scripts/changeScenes.cs
+++ HiScore/Assets/Scripts/changeScenes.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class changeScenes : MonoBehaviour
 {
+	public static string sceneName = "";
+
     // Start is called before the first frame update
     public void change(string NextScene)
     {
@@ -13,10 +16,10 @@ public class changeScenes : MonoBehaviour
     //previous scene
     public void retryLevel()
     {
-       
+       	//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
         int previousLevel = PlayerPrefs.GetInt("previousLevel");
-        Application.LoadLevel(previousLevel);
- 
+        //Application.LoadLevel(previousLevel);
+        SceneManager.LoadScene(previousLevel);
     }
 
     //exit
